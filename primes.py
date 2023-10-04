61ca8060f4e1392712f9fa926b121cb6bc1cc95c
Sam Thomas <samuel.mmthomas@gmail.com>
2023-10-04 14:48:29 +0100

    Add functionality to now allow a length of 0 or less

--- primes.py
+++ primes.py
@@ -2,5 +2,21 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError("The list must be of a length above 0.")
+
     list = []
+    index = 2
+    while number_of_primes > 0:
+        if checkPrime(index):
+            list.append(index)
+            number_of_primes -= 1
+        index += 1
     return list
+
+def checkPrime(number):
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+    return True
+
